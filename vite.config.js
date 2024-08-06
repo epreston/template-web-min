0d63a380a87b01b9173f09fe0a941bb9315b6a37
Ed Preston <epreston@prestonsoft.com>
2024-08-07 07:33:19 +1000

    Update vite.config.js
    add note for common setting

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    // minify: false,
     assetsInlineLimit: 0,
     target: ['es2022']
   }
